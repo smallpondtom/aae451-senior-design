e19c7078787906fc568578f63f1537b78e8601ee
smallpondtom <usasmpdtom@gmail.com>
2021-02-05 18:33:55 -0500

    Add data and environment.yml from Prof. Goppert repo and create few matlab files.

---
+++ src/matlab/imperial2metric.m
@@ -0,0 +1,53 @@
+function [x, u] = imperial2metric(val, unit)
+%{ 
+    This file converts imperial units to metric 
+    input << 
+    val : value to convert 
+    unit : the unit (in imperial) of the given value
+    
+    output >> 
+    x : the converted value 
+    u : the metric unit 
+%}
+
+% Array of possible imperial unit input 
+possible_units = ['lb','lbf','ft','in','hp','ft2','ft3','ft/s'];
+
+% Assertion for input validation 
+assert(any(possible_units(:) == unit), 'The only possible imperial units are lb, ft, in, hp, ft^2, ft^3, and ft/s.');
+idx = find(possible_units == unit);  % the corresponding index in the possible_units array for the unit input
+
+if idx == 1
+    % convert lb to kg
+    x = 0.453592 * val;
+    u = 'kg';
+elseif idx == 2
+    % convert lbf to N 
+    x = 4.44822 * val;
+    u = 'N';
+elseif idx == 3
+    % convert ft to m
+    x = 0.3048 * val;
+    u = 'm';
+elseif idx == 4
+    % convert in to m
+    x = 0.0254 * val;
+    u = 'm';
+elseif idx == 5
+    % convert hp to W or J/s
+    x = 745.7 * val;
+    u = 'W';
+elseif idx == 6
+    % convert ft^2 to m^2
+    x = 0.092903 * val;
+    u = 'm2';
+elseif idx == 7
+    % Convert ft^3 to m^3
+    x = 0.0283168 * val;
+    u = 'm3';
+else
+    % convert ft/s to m/s 
+    x = 0.3048 * val;
+    u = 'm/s';
+end
+end
---
+++ src/matlab/sutherland_visc_calc.m
@@ -0,0 +1,36 @@
+function myu = sutherland_visc_calc(T, gas)
+%% SUTHERLAND_VISC_CALC Sutherland's Viscosity Formula
+%% Theory
+% Sutherland's Viscosity Formula is a approximation of the viscosity depending 
+% on the temperature of a certain gas.
+% 
+% $$\mu = \mu_{ref}(\frac{T}{T_{ref}})^{1.5}(\frac{T_{ref} + C}{T + C})$$
+% 
+% here, $T_{\textrm{ref}\;} =288\ldotp 15\;K=\;{15}^o \;C\;$ and $\mu_{\textrm{ref}} 
+% =1\ldotp 789\times {10}^{-5} \;\;\;\;\textrm{Pa}-s$
+% 
+% and $C$ is the Sutherland Constant which is specific for each gas.
+    if gas == "air"
+        C = 120;
+    elseif gas == "NH3"
+        C = 370;
+    elseif gas == "CO2"
+        C = 240;
+    elseif gas == "CO"
+        C = 118;
+    elseif gas == "H2"
+        C = 72;
+    elseif gas == "N2"
+        C = 111;
+    elseif gas == "O2"
+        C = 127;
+    elseif gas == "SO2"
+        C = 416;
+    else 
+        disp("Error. Choose an appropriate gas.")
+    end
+    % Calculation
+    T_ref = 288.15;  % [K]
+    myu_ref = 1.789 * 10^(-5);  % [Pa-s]
+    myu = myu_ref * (T / T_ref)^(1.5) * (T_ref + C) / (T + C);
+end
