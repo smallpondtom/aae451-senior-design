c2e049643cd5cbf2924e9dec89cba648b773d00e
DRK98519 <panlongxuderek@gmail.com>
2021-02-18 04:58:07 -0500

    Comment Output Parameter

--- src/matlab/ClimbFunction.m
+++ src/matlab/ClimbFunction.m
@@ -8,5 +8,5 @@ function output = ClimbFunction(inputs)
 % Historical climb fuel weight fraction (Raymer Ch.3, Table 3.2)
 % "inputs" not used here
 
-output.f_cl = 0.985;
+output.f_cl = 0.985; % Remaining Fuel Weight / Beginning Fuel Weight at Climb Phase
 end
