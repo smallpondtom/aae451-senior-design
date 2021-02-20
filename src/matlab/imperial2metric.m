c94e9849dfb285b92423741598b0ed9bc6d90700
DRK98519 <panlongxuderek@gmail.com>
2021-02-20 02:27:11 -0500

    Fix string issue of Possible_Units array

--- src/matlab/imperial2metric.m
+++ src/matlab/imperial2metric.m
@@ -11,11 +11,10 @@ function x = imperial2metric(val, unit)
 %}
 
     % Array of possible imperial unit input 
-    possible_units = ['lb','lbf','ft','in','hp','ft2','ft3','ft/s','nmi','lb/ft^2','knot','slug/ft^3', 'lb/lbf-hr'];
+    possible_units = ["lb","lbf","ft","in","hp","ft2","ft3","ft/s","nmi","lb/ft^2","knot","slug/ft^3", "lb/lbf-hr"];
 
     % Assertion for input validation 
-    assert(any(possible_units(:) == unit), ...
-    'The only possible imperial units are lb, ft, in, hp, ft^2, ft^3, ft/s, nmi, lb/ft^2, knot, slug/ft^3','lb/lbf-hr');
+    assert(any(possible_units(:) == unit), 'The only possible imperial units are lb, ft, in, hp, ft^2, ft^3, ft/s, nmi, lb/ft^2, knot, slug/ft^3,lb/lbf-hr');
     idx = find(possible_units == unit);  % the corresponding index in the possible_units array for the unit input
 
     if idx == 1
@@ -47,7 +46,7 @@ function x = imperial2metric(val, unit)
         x = 1852 * val;
     elseif idx == 10
         % convert lb/ft^2 to kg/m^2
-        x = 4.88243 * val;
+        x = 4.88242764 * val;
     elseif idx == 11
         % convert knot to m/s
         x = 0.514444 * val;
