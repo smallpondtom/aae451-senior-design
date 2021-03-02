fbbb06ef14f5acd089af6ad06b5f1483abb9c3b7
smallpondtom <usasmpdtom@gmail.com>
2021-03-01 21:23:07 -0500

    Move files.

--- src/matlab/readJSON2struct.m
+++
@@ -1,8 +0,0 @@
-function s = readJSON2struct(fname)
-    % This function reads a JSON file as a structure 
-    fid = fopen(fname); 
-    raw = fread(fid,inf); 
-    str = char(raw'); 
-    fclose(fid); 
-    s = jsondecode(str);
-end
