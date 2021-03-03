7d18add97b78f9ed711b4ed17c57c206f879a699
Austin Grant Williams <will1560@purdue.edu>
2021-03-02 21:15:04 -0500

    Further updated aircraft geometry

--- src/matlab/sizing_code/runMASC_Commercial.m
+++ src/matlab/sizing_code/runMASC_Commercial.m
@@ -41,19 +41,19 @@ PerformanceInputs.M    = ...
     PerformanceInputs.V / PerformanceInputs.cruiseSpeedOfSound;  % cruise velocity [Mach]. 
 
 %% GEOMETRY PARAMETERS
-GeometryInputs.AR          = 9;     % wing aspect ratio
+GeometryInputs.AR          = 8.91;     % wing aspect ratio
 GeometryInputs.WingSweep   = 9.519;    % wing sweep, Lambda (LE) [deg]
-GeometryInputs.HtSweep     = 35;      % horizontal tail sweep angle [deg]
-GeometryInputs.VtSweep     = 40;      % vertical tail sweep angle [deg]
+GeometryInputs.HtSweep     = 21;      % horizontal tail sweep angle [deg]
+GeometryInputs.VtSweep     = 45;      % vertical tail sweep angle [deg]
 GeometryInputs.thick2chord = 0.12;    % wing thickness-to-chord ratio
-GeometryInputs.t2c_ht      = 0.1;     % horizontal tail thickness-to-chord ratio
-GeometryInputs.t2c_vt      = 0.1;     % vertical tail thickness-to-chord ratio
-GeometryInputs.TR          = 0.3;     % wing taper ratio
+GeometryInputs.t2c_ht      = 0.14;     % horizontal tail thickness-to-chord ratio
+GeometryInputs.t2c_vt      = 0.06;     % vertical tail thickness-to-chord ratio
+GeometryInputs.TR          = 0.33;     % wing taper ratio
 GeometryInputs.FinessRatio = 6.91;      % fuselage finess ratio
 GeometryInputs.AR_ht       = 6.4;     % horizontal tail aspect ratio
 GeometryInputs.AR_vt       = 1.3;     % vertical tail aspect ratio
-GeometryInputs.TR_ht       = 0.4;    % horizontal tail taper ratio
-GeometryInputs.TR_vt       = 0.57;    % vertical tail taper ratio
+GeometryInputs.TR_ht       = 0.5;    % horizontal tail taper ratio
+GeometryInputs.TR_vt       = .9346;    % vertical tail taper ratio
 GeometryInputs.k           = imperial2metric(2.08e-5, 'ft');    % Skin roughness value [m]
 GeometryInputs.A_max       = imperial2metric((0.25*pi*(6.25^2)), 'ft^2');         % fuselage maximum cross sectional area [m^2]
 GeometryInputs.Q_fuse      = 1.4;     % Fuselage-Nacelle interference factor
@@ -62,8 +62,8 @@ GeometryInputs.Q_nacel     = 1.2;     % Nacelle-Wing interference factor
 GeometryInputs.Q_ht        = 1.05;    % Horizontal tail interference factor
 GeometryInputs.Q_vt        = 1.05;    % Vertical tail interference factor 
 GeometryInputs.upsweep     = 5;       % Upsweep angle of tail [deg]
-GeometryInputs.L_nacel     = 2;       % Nacelle length [m]
-GeometryInputs.D_nacel     = 1;       % Nacelle diamater (m)
+GeometryInputs.L_nacel     = 3.3;       % Nacelle length [m]
+GeometryInputs.D_nacel     = 1.3;       % Nacelle diamater (m)
 
 %% CONFIGURATION PARAMETERS
 % These parameters and their default values are listed in the LayoutFunction.m file
