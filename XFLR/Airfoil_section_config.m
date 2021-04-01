8815edb0a43dcde662880715e5bb5c790d0dce44
DRK98519 <panlongxuderek@gmail.com>
2021-03-31 23:36:09 -0400

    Airfoil data

--- XFLR/Airfoil_section_config.m
+++ XFLR/Airfoil_section_config.m
@@ -1,12 +1,17 @@
+%% Wing Config
+x = [0 1.5];
+y = [0 6.958];
+c = [2.4 0.77];
+coef_chord = polyfit(y, c, 1);
+coef = polyfit(y, x, 1);
 
 %% Fin Config
-x = [-0.5 1.5]; y = [0; 2.2];
-c = [3 1.5];
-coef_chord = polyfit(y, c, 1); % Chord length function wrt y
-
-coef = polyfit(x,y,1); % Airfoil tip coord y function wrt x
+%x = [-0 1.5]; y = [0; 6.958]; % 
+%c = [2.4 0.77];
+%coef_chord = polyfit(y, c, 1); % Chord length function wrt y
+%coef = polyfit(x,y,1); % Airfoil tip coord y function wrt x
 
 %% Elevator Config
-x = [0, 1.5]; y = [0, 3.2]; c = [2, 1];
-coef_chord = polyfit(y, c, 1); % Chord length function wrt y
-coef = polyfit(x,y,1); % Airfoil tip coord y function wrt x
+%x = [0, 1.5]; y = [0, 3.2]; c = [2, 1];
+%coef_chord = polyfit(y, c, 1); % Chord length function wrt y
+%coef = polyfit(x,y,1); % Airfoil tip coord y function wrt x
